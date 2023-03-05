808ddd3d1e614db6efc04bf05ffee365d0d61b0c
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-03-05 08:42:24 +0100

    .

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,7 +1,13 @@
-// $(function () { // Same as document.addEventListener("DOMContentLoaded"...
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
-//   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-//   $("#navbarToggle").blur(function (event) {
-//       $("#offcanvasBody").collapse('hide');
-//   });
-// });
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+      $("#offcanvasBody").collapse('hide');
+  });
+});
+var showLoading = function (selector) {
+    var html = "<div class='text-center'>";
+    html += "<img src='IMAGES/1485.gif'></div>";
+    insertHtml(selector, html);
+  };
+  showLoading("#main-content");
