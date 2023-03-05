77f0a1b150ca2e9a2282a4a3c960d5ca94ee8ec3
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-03-05 08:37:25 +0100

    .

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,7 @@
+// $(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+//   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+//   $("#navbarToggle").blur(function (event) {
+//       $("#offcanvasBody").collapse('hide');
+//   });
+// });
