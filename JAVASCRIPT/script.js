c2e54b70b016390bb6d0905935f6195b56436aa0
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-03-13 16:09:32 +0100

    .

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -40,3 +40,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
       x.style.display = "block";
     }
   }
+  let mybutton = document.getElementById("myBtn");
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+    const d = new Date ()
+    let year = d.getFullYear();
+    document.getElementById("year").innerHTML = year;
